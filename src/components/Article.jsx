32b3f79b066370dacf3d269e201b621e49af1ac9
TR0205 <index0205@gmail.com>
2022-07-03 14:01:51 +0900

    state学習

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { useState } from "react";
+import PublishButton from "./PublishButton";
 
 const Article = (props) => {
+    const [isPublished, setIsPublished] = useState(false);
+    const publishArticle = () => {
+        setIsPublished(true);
+    }
+    // console.log(isPublished);
     return (
         <>
             <h2>{props.title}</h2>
             <p>{props.content}</p>
+            <PublishButton isPublished={isPublished} onClick={publishArticle} />
         </>
     )
 }
