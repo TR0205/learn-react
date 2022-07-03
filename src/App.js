91ba1b814cc8bb12a7daaf43dfedcb2a1589486d
TR0205 <index0205@gmail.com>
2022-07-03 21:00:51 +0900

    コンポーネント追加

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import Article from "./components/Article";
+import ToggleButton from "./components/ToggleButton";
 
 function App() {
     return (
         <>
-            <Article
-                title={'新・日本一わかりやすい'}
-                content={'コンテント'}
-            />
+            <ToggleButton />
         </>
     );
 }
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const TextInput = () => {
+    const [name, setName] = useState('');
+    const handleName = (event) => {
+        setName(event.target.value);
+    }
+
+    console.log(name)
+
+    return (
+        <input
+            onChange={(event) => handleName(event)}
+            type={'text'}
+            value={name}
+        />
+    );
+};
+
+export default TextInput;
